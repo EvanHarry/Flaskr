899d1530a0331c78219431ab5b98f9271eefee90
Evan Harry <evan.harry@apg-aarl.co.uk>
2017-07-03 23:19:10 +0100

    Add more config options for flaskr

--- config.py
+++ config.py
@@ -13,8 +13,19 @@ class DevelopmentConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgres://postgres:6ktxT9@127.0.0.1:5432/flaskr'
 
 
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'postgres://postgres:6ktxT9@127.0.0.1:5432/flaskr_test'
+
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgres://evan:imfaa#18@127.0.0.1:5432/flaskr'
+
+
 config = {
     'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
 
     'default': DevelopmentConfig
 }
--- manage.py
+++ manage.py
@@ -6,6 +6,12 @@ from flask_migrate import Migrate, MigrateCommand
 from app import create_app, db
 from app.models import User, Task
 
+if os.path.exists('.env'):
+    for line in open('.env'):
+        var = line.strip().split('=')
+        if len(var) == 2:
+            os.environ[var[0]] = var[1]
+
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
 migrate = Migrate(app, db)
