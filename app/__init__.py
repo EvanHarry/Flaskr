855919c3ffbd0ab1ff525dadf04ae3b6c21ccf79
Evan Harry <evan.harry@apg-aarl.co.uk>
2017-07-18 23:21:48 +0100

    Start work on admin site

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,16 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
 from config import config
 
+bootstrap = Bootstrap()
 db = SQLAlchemy()
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'admin.index'
 
 
 def create_app(config_name):
@@ -11,15 +18,20 @@ def create_app(config_name):
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
 
+    bootstrap.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
 
     from .main import main_blueprint
     app.register_blueprint(main_blueprint)
 
     from .user import user_blueprint
-    app.register_blueprint(user_blueprint)
+    app.register_blueprint(user_blueprint, url_prefix='/api/v1.0')
 
     from .task import task_blueprint
-    app.register_blueprint(task_blueprint)
+    app.register_blueprint(task_blueprint, url_prefix='/api/v1.0')
+
+    from .admin import admin_blueprint
+    app.register_blueprint(admin_blueprint, url_prefix='/admin')
 
     return app
--- app/models.py
+++ app/models.py
@@ -1,11 +1,12 @@
 from flask import current_app
+from flask_login import UserMixin
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer, BadSignature, SignatureExpired
 from werkzeug.security import generate_password_hash, check_password_hash
 
-from . import db
+from . import db, login_manager
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
@@ -63,6 +64,11 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
 class Task(db.Model):
